0cbd8c2ff775d20df68bf64b45653993dbb18e3f
userm2 <79088230003@yandex.ru>
2020-10-05 20:05:54 +0500

    in cube

--- src/m01/s19/Task4.java
+++ src/m01/s19/Task4.java
@@ -8,6 +8,18 @@ public class Task4 {
 
     }
     static double returnSomeSheat(double x) {
-
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        for (int i = 0; i < n; i++) {
+            x = x * 2;
+            System.out.print(x * power(10, i) / 10);
+        }
+    }
+    public static int power(int value, int powValue) {
+        int result = 1;
+        for (int i = 1; i <= powValue; i++) {
+            result = result * value;
+        }
+        return result;
     }
 }
